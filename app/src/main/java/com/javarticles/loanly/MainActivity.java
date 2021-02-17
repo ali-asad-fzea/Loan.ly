e0da3c47c59a0be1844fe40377649df106ed936c
RahulSChauhan50 <rahulschauhan50@gmail.com>
2021-02-18 01:05:28 +0530

    Created Image file and specified saving location

--- app/src/main/java/com/javarticles/loanly/MainActivity.java
+++ app/src/main/java/com/javarticles/loanly/MainActivity.java
@@ -1,11 +1,26 @@
 package com.javarticles.loanly;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
     TextView image3data;
     TextView image4data;
     Button submit;
+    Bitmap[] sampleimages;
+    String[] sampleimagesinfo;
+
+    int imagenumber=-1;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +59,101 @@ public class MainActivity extends AppCompatActivity {
         image3data=findViewById(R.id.textViewimage3);
         image4data=findViewById(R.id.textViewimage4);
         submit=findViewById(R.id.submit);
+
+        sampleimages= new Bitmap[]{null, null, null, null};
+        sampleimagesinfo=new String[]{null,null,null,null};
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                submit_function();
+            }
+        });
+
+        selectphoto1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagenumber=0;
+                capture_function();
+            }
+        });
+
+        selectphoto2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagenumber=1;
+                capture_function();
+            }
+        });
+
+        selectphoto3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagenumber=2;
+                capture_function();
+            }
+        });
+
+        selectphoto4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagenumber=3;
+                capture_function();
+            }
+        });
     }
+
+    void submit_function(){
+
+    }
+    void capture_function(){
+        final int REQUEST_IMAGE_CAPTURE = 1;
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                        "com.javarticles.loanly.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+
+
+    }
+
+
+    //creating a temporary file for image
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        //storing filename for display
+        if(imagenumber!=-1){
+            sampleimagesinfo[imagenumber]=imageFileName+".jpg ";
+        }
+
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
 }
